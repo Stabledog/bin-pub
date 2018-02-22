db37a9179c4ed20108d9f2b3d170645d02dcaf6e
Les Matheson <lmatheson4@bloomberg.net>
2018-02-22 10:04:33 -0500

    sync

--- tox-py/test_tox.py
+++ tox-py/test_tox.py
@@ -3,8 +3,13 @@ from tox_core import *
 def test1():
     loadIndex()
 
+def test2():
+    pm=PathMatcher.find_matching_paths( [ '/ab/bc/cd', '/x/y/z'], '*c' )
+    print(pm)
+
 if __name__=="__main__":
 
+    test2()
     test1()
 
 

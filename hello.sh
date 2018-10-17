b0a4c147777d448a7bcf0cb52eb0896bc25f7529
Les Matheson <les.matheson@gmail.com>
2018-10-17 07:27:09 -0400

    sync

--- hello.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script prints strings from bash history which match a pattern passed by the command line, but does not print the history line number.
-
-
-# Hello again
-
-This is the real file don't be fooled!!

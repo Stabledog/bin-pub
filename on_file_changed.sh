17ec9813d828c3339468eb6d9659e8ddf2a424ed
Les Matheson <les.matheson@gmail.com>
2020-03-07 11:05:38 -0500

    Fix on_file_changed.sh

--- on_file_changed.sh
+++ on_file_changed.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-#  Run a command when file changes.  Prompts user for
-# command (via cat) which can include piping, etc.
-#  $1 is the name of the file to be watched
+#  Usage:  [-] file-to-watch
+#  If '-' is the first argument, command will be read from stdin with
+#  no prompting.  Otherwise we'll prompt the user for the command to
+#  run when file-to-watch metadata changes (mode, size, timestamp, existence)
+#
+#  Because we treat existence like another bit of metadata, we'll trigger if
+#  the file comes into or goes out of existence, just as if the timestamp or
+#  size had changed.  This means if you misspell the filename, you'll have
+#  a hidden problem.
 
 function errExit {
     echo "ERROR: $@" >&2
@@ -16,18 +22,29 @@ function get_cmd_from_user {
     cmd="$(cat)"
 }
 
-[[ -f $1 ]] || errExit "Can't find file $1"
+function file_metadata {
+    ls -l --full-time $1 2>&1
+}
 
-old_meta="$(ls -l $1)"
+if [[ "$1" == - ]]; then
+    # Get command from stdin with no user prompt:
+    shift
+    cmd="$(cat)"
+else
+    # Prompt user for command:
+    get_cmd_from_user $1
+fi
+
+
+old_meta="$(file_metadata $1)"
 new_meta="$old_meta"
-get_cmd_from_user weasl.txt
 
 while true; do
     while [[ "$new_meta" == "$old_meta" ]]; do
         sleep 1
-        new_meta="$(ls -l $1)"
+        new_meta="$(file_metadata $1)"
     done
     eval "$cmd"
-    old_meta=new_meta
+    old_meta="$new_meta"
 done
 

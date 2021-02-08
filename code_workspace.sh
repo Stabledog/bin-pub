199fc69a1ea8516802c73bb65f576bcc6a8b2da7
Les Matheson <lmatheson4@bloomberg.net>
2021-02-08 07:55:43 -0500

    Fix quoting in code_workspace.sh

--- code_workspace.sh
+++ code_workspace.sh
@@ -5,6 +5,13 @@
 # workspace file, to ensure that ${WorkspaceFolder} matches the CWD for
 # vscode itself.
 
+Code="$(which code)"
+
+die() {
+    echo "ERROR: $@" >&2
+    exit 1
+}
+
 function code_ws {
 
     [[ -n $1 ]] || return $(die Please supply a workspace file as \$1)
@@ -21,7 +28,7 @@ function code_ws {
         cd $ws_dir || die "Can't cd to $ws_dir"
         xenv=$(cat $env_filename)
         echo "xenv=[$xenv]"
-        cmd="$xenv code $ws_filename"
+        cmd="$xenv \"$Code\" $ws_filename"
         echo -e "\033[;33m${cmd}\[033;0m"
         eval "$cmd $@"
     )

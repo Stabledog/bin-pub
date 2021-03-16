0c5af30f07f29905954e1cad0fc1abd5cf81d96f
Les Matheson <lmatheson4@bloomberg.net>
2021-03-16 11:08:59 -0400

    Add task-find.sh

---
+++ task-find.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# task-find.sh
+#  Track down aliases, functions, scripts...
+#  Configuration:
+#     Add ~/.taskrc/task-find-dirs to extend the list of directories searched beyond "find_dirs"
+
+die() {
+    echo "ERROR: $@"
+    exit 1
+}
+
+yellow() {
+    echo -e "\033[;33m$@\033[;0m"
+}
+
+find_dirs="$PWD $HOME/.taskrc $HOME/.taskrc/bin $HOME/bin $(cat ~/.taskrc/task-find-dirs 2>/dev/null)"
+
+if [[ -z $sourceMe ]]; then
+    expr="$1"
+    for xdir in $find_dirs; do
+        cd $xdir || { echo "ERROR: Can't cd to $xdir" >&2; continue; }
+        yellow "Searching $PWD for \"${expr}\":" >&2
+        grep -E "$expr" * 2>/dev/null | sed "s%^% $PWD/%"
+        #grep -E "$expr" * 2>/dev/null
+    done
+fi

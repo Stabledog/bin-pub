6fea14ad8243a2b4fdfd4e7ed8a90999c5785d15
Les Matheson <lmatheson4@bloomberg.net>
2021-06-17 21:05:00 -0400

    Prompt user for workspace file

--- code_workspace.sh
+++ code_workspace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Help Launch vscode in custom env.
-# Each workspace file should have a corresponding [workspace].env file in the
+# Each workspace file may have a corresponding [workspace].env file in the
 # same dir.  We source it and then run vscode from the dir containing the
 # workspace file, to ensure that ${WorkspaceFolder} matches the CWD for
 # vscode itself.
@@ -12,28 +12,40 @@ die() {
     exit 1
 }
 
-function code_ws {
+choose_wsfile() {
+    exec 9< <(echo "0 <none>"; ls *.code-workspace vscode/*.code-workspace 2>/dev/null | sed 's/\.code-workspace//' | cat -n )
+    while read -u 9 line; do
+        echo $line >&2
+    done
+    exec 9<&-
+    read -n 1 -p "Select workspace #: "
+    ls *.code-workspace vscode/*.code-workspace  2>/dev/null | sed "${REPLY}q;d"
+}
 
-    [[ -n $1 ]] || return $(die Please supply a workspace file as \$1)
-    local ws_name=$(readlink -f -- $1 | sed 's/\.code-workspace//')
-    shift
+function main {
+    local wsfile=${1}
+    if [[ -z $wsfile ]]; then
+        wsfile=$(choose_wsfile)
+        [[ -z $wsfile ]] && return $(die Please supply a workspace file as \$1)
+    fi
+    local ws_name=${wsfile//\.code-workspace}
     local ws_filename=${ws_name}.code-workspace
     [[ -f $ws_filename ]] || return $(die "Can't find $ws_filename")
     local env_filename="${ws_name}.env"
-    if [[ ! -f $env_filename ]]; then
-        read -p "Can't find $env_filename, so I can't source it.  Hit Enter to continue anyway."
-    fi
     (
+
         ws_dir=$(dirname $ws_filename)
-        cd $ws_dir || die "Can't cd to $ws_dir"
         xenv=$(cat $env_filename)
+        cd $ws_dir || die "Can't cd to $ws_dir"
         echo "xenv=[$xenv]"
-        cmd="$xenv \"$Code\" $ws_filename"
+        cmd="$xenv \"$Code\" $(basename $ws_filename)"
         echo -e "\033[;33m${cmd}\[033;0m"
         eval "$cmd $@"
     )
 }
 
 if [[ -z $sourceMe ]]; then
-    code_ws "$@"
+    #code_ws "$@"
+    main  "$@"
+    #choose_wsfile "$@"
 fi

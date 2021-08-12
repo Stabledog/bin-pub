897e68581ac92b4a7e421ef3dc7262921c6bc29c
Les Matheson <lmatheson4@bloomberg.net>
2021-08-12 07:43:29 -0400

    Add memclean to localhist

--- localhist_completion.bash
+++ localhist_completion.bash
@@ -1,7 +1,7 @@
 # localhist_completion.bash
 
 _localhist_commands() {
-    echo -e "help on join write export read import off show dump edit clear clean compare merge grep catalog"
+    echo -e "help on join write export read import off show dump edit clear clean memclean compare merge grep catalog"
 }
 
 _localhist_completion() {

cab9015bee62e87875a9cd2200009c3cc9b94fae
Les Matheson <lmatheson4@bloomberg.net>
2021-08-24 08:21:23 -0400

    Refine valid python versions for path_undupe

--- path_undupe.bash
+++ path_undupe.bash
@@ -4,7 +4,7 @@
 path_undupe() {
     # Replace the value of PATH with ordered/unduplicated equivalent
     command which path_undupe.py &>/dev/null || return;
-    local mypy=$(command which python3.9 python3.8 python3.7 python3.6 python3.5 | command head -n 1)
+    local mypy=$(command which python3.9 python3.8 python3.7 | command head -n 1)
     [[ -z $mypy ]] && return;
     PATH=$( ${mypy} $(command which path_undupe.py) --unique )
 }

4dabc0be2fe9315a6d819b91c2f29e17342f81a9
Les Matheson <lmatheson4@bloomberg.net>
2021-08-25 16:43:33 -0400

    Fix bug in path_undupe.bash

--- path_undupe.bash
+++ path_undupe.bash
@@ -4,7 +4,7 @@
 path_undupe() {
     # Replace the value of PATH with ordered/unduplicated equivalent
     command which path_undupe.py &>/dev/null || return;
-    local mypy=$(command which python3.9 python3.8 python3.7 | command head -n 1)
+    local mypy=$(command which python3.9 python3.8 python3.7 2>/dev/null | command head -n 1)
     [[ -z $mypy ]] && return;
     PATH=$( ${mypy} $(command which path_undupe.py) --unique )
 }

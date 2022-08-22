6ed82ee88e64eac6de8a015ce6e05ade8e890e49
Les Matheson <les.matheson@gmail.com>
2022-08-22 06:38:57 -0400

    sync

--- vscode-make-snippet.sh
+++ vscode-make-snippet.sh
@@ -16,7 +16,7 @@ stub() {
 }
 
 xform_body_line() {
-    command sed -e 's/["]/\\"/g' -e 's/[$]/\\\$/g' -e 's/^/"/' -e 's/$/",/'
+    command sed -e 's/["]/\\"/g' -e 's/[$]/\\\\\$/g' -e 's/^/        "/' -e 's/$/",/'
 }
 emitHeader() {
     command cat <<-EOF
@@ -30,21 +30,27 @@ EOF
 emitTail() {
     command cat <<-EOF
     ]
-}
+},
 EOF
 }
 
-main() {
+make_snippet() {
     emitHeader
-    echo "Enter raw body text and then hit Ctrl+D:" >&2
+    [[ -t 0 ]] && echo "Enter raw body text and then hit Ctrl+D:" >&2
     (
-        command stty -echo
+        [[ -t 0 ]] && command stty -echo
         while IFS= builtin read line; do
             xform_body_line <<< "$line"
         done
-        command stty echo
+        [[ -t 0 ]] && command stty echo
     )
     emitTail
+}
+
+main() {
+    local outfile=/tmp/_new_snippet.json
+    make_snippet | sed 's/^/    /' | command tee $outfile
+    echo "Snippet output copied to $outfile" >&2
 }
 
 [[ -z ${sourceMe} ]] && {

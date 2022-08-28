1c3aa6d04f014f468720f2720bd89edbe541b360
Les Matheson <les.matheson@gmail.com>
2022-08-28 05:29:30 -0400

    Add github-gh-install-debian.sh

--- github-gh-install-debian.sh
+++ github-gh-install-debian.sh
@@ -15,6 +15,7 @@ stub() {
 }
 main() {
     builtin echo "args:[$*]"
+
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
         | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
      && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \

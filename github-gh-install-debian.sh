eb0cb4cfe9dca984d3a3fd1b9cbb960fd9e3b950
Les Matheson <les.matheson@gmail.com>
2022-08-28 05:27:38 -0400

    Add github-gh-install-debian.sh

---
+++ github-gh-install-debian.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# github-gh-install-debian.sh
+#  See also: https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
+
+scriptName="$(readlink -f "$0")"
+scriptDir=$(command dirname -- "${scriptName}")
+
+die() {
+    builtin echo "ERROR($(basename ${scriptName}): $*" >&2
+    builtin exit 1
+}
+
+stub() {
+   builtin echo "  <<< STUB[$*] >>> " >&2
+}
+main() {
+    builtin echo "args:[$*]"
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+     && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+     && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
+        | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+     && sudo apt update \
+     && sudo apt install gh -y
+     [[ $? -eq 0 ]] || die
+
+     echo "Installed: OK" >&2
+     echo "Run \"gh auth login\" to continue with authentication setup " >&2
+
+}
+
+[[ -z ${sourceMe} ]] && {
+    main "$@"
+    builtin exit
+}
+command true

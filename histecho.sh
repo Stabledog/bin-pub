84591b70945334ad9835b17762c789d90110e78e
Les Matheson <lmatheson4@bloomberg.net>
2022-09-14 12:52:59 -0400

    Initial histecho.sh

--- histecho.sh
+++ histecho.sh
@@ -23,6 +23,9 @@ stripTimestamps=true
 EOF
 }
 
+loop_mode=false
+loop_interval=30
+
 stub() {
     # Print debug output to stderr.  Call like this:
     #   stub "${FUNCNAME[0]}.${LINENO}" "$@" "<Put your message here>"
@@ -43,6 +46,9 @@ parseArgs() {
                 #  do_help $*
                 exit 1
                 ;;
+            -l|--loop)
+                loop_mode=true
+                ;;
             --config)
                 # Print default config file
                 makeDefaultConfig
@@ -54,10 +60,27 @@ parseArgs() {
         esac
         shift
     done
+    source $configFile
+}
+
+do_send() {
+    tac $sourcePath | ssh ${destHost} bash -c "cat > ${targetPath}"
+}
+
+do_loop_send() {
+    while true; do
+        set -x
+        do_send
+        set +x
+        sleep $loop_interval
+    done
 }
 
 main() {
     parseArgs "$@"
+    if $loop_mode; then
+        do_loop_send
+    fi
 }
 
 [[ -z ${sourceMe} ]] && {

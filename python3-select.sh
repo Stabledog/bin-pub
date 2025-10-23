ab81be431bdafe7a711fed9d3502459e086be85a
Les Matheson <lmatheson4@bloomberg.net>
2025-10-23 13:15:40 +0000

    Fix old python3-select logic

--- python3-select.sh
+++ python3-select.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
+#shellcheck disable=2154
+PS4='$( _0=$?; exec 2>/dev/null; realpath -- "${BASH_SOURCE[0]:-?}:${LINENO} ^$_0 ${FUNCNAME[0]:-?}()=>" ) '
+
 
 export BEST_PY3
 
 python3_select() {
     local cand;
-    for cand in python3.{13..6} python3 python; do
+    for cand in python3.{14..9} python3 python; do
         local py_cand="$( command which ${cand} 2>/dev/null)"
+        [[ -n $py_cand ]] || continue
 	    [[ -n $MSYS ]] && {
             "$py_cand" -c 'import sys; sys.exit(0)' &>/dev/null || continue
-        } || {
-            # On Unix-ish shells, avoid a Windows versions of python:
-            "$py_cand" -c 'import termios' &>/dev/null || \
-		    continue  # We don't want a dos/windows version
+        } 
+        [[ $py_cand == *python ]] && {
+            # We can't use v2:
+            local xver=$( python --version 2>/dev/null)
+            [[ "$xver" == Python\ 3* ]] || continue
         }
         BEST_PY3="${py_cand}"
         break
     done
     [[ -n "$BEST_PY3" ]] || {
-        echo "ERROR: no python3 on path" >&2; false; return;
+        echo "ERROR: no usable python3 on path" >&2; false; return;
     }
     echo "$BEST_PY3"
 }
